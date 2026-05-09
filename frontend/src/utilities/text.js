712ca648177df0a0a01301a2e57f425389d60076
Sushant <sus.hant@outlook.com>
2026-05-10 01:29:18 +1000

    Adds capitalize utility function with tests

--- frontend/src/utilities/text.js
+++ frontend/src/utilities/text.js
@@ -1,4 +1,4 @@
-export function capitalize(str){
-    if(str === undefined || str === null  || str.length === 0) return "";
-    return str[0].toUpperCase() + str.slice(1);
+export function capitalize(str) {
+  if (str === undefined || str === null || str.length === 0) return '';
+  return str[0].toUpperCase() + str.slice(1);
 }
