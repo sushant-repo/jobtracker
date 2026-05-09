00b630acf604998350d7f57a37014bd6d5b9555c
Sushant <sus.hant@outlook.com>
2026-05-10 00:50:20 +1000

    Adds prettier with eslint and also installs bootstrap css library

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,8 +1,10 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import prettierConfig from 'eslint-config-prettier';
+import prettierPlugin from 'eslint-plugin-prettier';
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -12,6 +14,15 @@ export default defineConfig([
       js.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
+      prettierConfig,
+      {
+        plugins: {
+          prettier: prettierPlugin,
+        },
+        rules: {
+          'prettier/prettier': 'error',
+        },
+      },
     ],
     languageOptions: {
       ecmaVersion: 2020,
@@ -26,4 +37,4 @@ export default defineConfig([
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
     },
   },
-])
+]);
